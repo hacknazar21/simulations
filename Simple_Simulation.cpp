da9880d3cc2ef9e0ef43dc3098900df2de8dd24f
hacknazar21 <nazarcool2001@gmail.com>
2020-05-25 11:51:53 +0600

    Алгоритм быстрого поиска

--- Simple_Simulation.cpp
+++ Simple_Simulation.cpp
@@ -1,9 +1,18 @@
 #include "connections.hpp"
-#include "Simple_Simulation.hpp"
-#include "functions.hpp"
 
-int main(int argc, char const *argv[])
+static const int N = 10000;
+
+int main()
 {
-	Start programm;
+	int i, p, q, id[N];
+	for (int i = 0; i < N; i++) id[i] = i;
+	while(std::cin >> p >> q) 
+	{
+	    int t = id[p];
+	    if (t == id[q]) continue;
+	    for (int i = 0; i < N; ++i)
+		   if (id[i] == t) id[i] = id[q];
+		std::cout << " " << p << " " << q << "\n";
+	}
 	return 0;
 }
