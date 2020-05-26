8a3d14b034e54d9ad35df9be1a78b5c6ef652a70
hacknazar21 <nazarcool2001@gmail.com>
2020-05-26 18:51:02 +0600

    Алгоритм взвешанного быстрого объединения

--- Simple_Simulation.cpp
+++ Simple_Simulation.cpp
@@ -4,19 +4,48 @@ static const int N = 10000;
 
 int main()
 {
-	int i, p, q, id[N];
-	for (int i = 0; i < N; i++) id[i] = i;
+	int i, j, p, q, id[N], sz[N];
+	for (i = 0; i < N; i++)  //0, 1, 2, ... N - 1
+	{ 	
+		id[i] = i; 
+		sz[i] = 1;
+	}
 	while(std::cin >> p >> q) 
 	{
+	    for (i = p; i != id[i]; i = id[i]);
+	    for (j = q; j != id[j]; j = id[j]);	    
+	    if (i == j)
+	    { 
+	    	std::cout << "Connected!" << "\n";
+	    	continue;
+	    }
+	    if (sz[i] < sz[j])
+	    {
+	    	id[i] = j; 
+	    	sz[j] += sz[i];
+	    }
+	    else
+	    {
+	    	id[j] = i; 
+	    	sz[i] += sz[j];
+	    }
+		std::cout << " " << p << " " << q << "\n";
+	}
+	return 0;
+}
+
+
+/*
 	    int t = id[p];
+
 	    if (t == id[q])
 	    { 
 	    	std::cout << "Connected!" << "\n";
 	    	continue;
 	    }
 	    for (int i = 0; i < N; ++i)
+	    {
 		   if (id[i] == t) id[i] = id[q];
+	    }
 		std::cout << " " << p << " " << q << "\n";
-	}
-	return 0;
-}
+*/
